d269b3fd8714e9cfa9a893bd2ea97b0eaef01944
Tibor <tibor736@hotmail.com>
2023-07-11 18:51:38 +0300

    01: created menu and menuItem components. Added data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import { useState } from "react";
+import menu from "./data";
+import Title from "./components/Title";
+import Menu from "./components/Menu";
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+	const [menuItems, setMenuItems] = useState(menu);
+
+	return (
+		<main>
+			<section className="menu">
+				<Title text="our menu" />
+				<Menu items={menuItems} />
+			</section>
+		</main>
+	);
 };
 export default App;
