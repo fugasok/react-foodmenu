5e9fd76b273e32974513bea4b2d26a2288680074
Tibor <tibor736@hotmail.com>
2023-07-11 19:27:38 +0300

    02: added menu filters

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,35 @@ import { useState } from "react";
 import menu from "./data";
 import Title from "./components/Title";
 import Menu from "./components/Menu";
+import Categories from "./components/Categories";
+
+//create menu filters
+
+// const tempCategories = menu.map((item) => item.category);
+// const tempSet = new Set(tempCategories);
+// const tempItems = ["all", ...tempSet];
+
+//one line create menu filter
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
 
 const App = () => {
 	const [menuItems, setMenuItems] = useState(menu);
+	const [categories, setCategories] = useState(allCategories);
+
+	const filterItems = (category) => {
+		if (category === "all") {
+			setMenuItems(menu);
+			return;
+		}
+		const newItems = menu.filter((item) => item.category === category);
+		setMenuItems(newItems);
+	};
 
 	return (
 		<main>
 			<section className="menu">
 				<Title text="our menu" />
+				<Categories categories={categories} filterItems={filterItems} />
 				<Menu items={menuItems} />
 			</section>
 		</main>
